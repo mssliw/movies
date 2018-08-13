47b4b9c1b75b8f187dbe201893cf230daa208776
mssliwinska <equinoxe@onet.eu>
2018-08-14 00:14:24 +0200

    db model init

--- movie_api/movie_api/settings.py
+++ movie_api/movie_api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'movies_app.apps.MoviesAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- movie_api/movies_app/models.py
+++ movie_api/movies_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    year = models.DateField(auto_now=False, auto_now_add=False)
+    description = models.TextField()
+
+
+class Comment(models.Model):
+    comment = models.TextField()
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE
+    )
+
+
