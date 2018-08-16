5a66801c191d1c122c76b7666f6d1417908cb446
mssliwinska <equinoxe@onet.eu>
2018-08-16 17:40:39 +0200

    db adjustments

--- movie_api/movies_app/models.py
+++ movie_api/movies_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=200)
-    year = models.DateField(auto_now=False, auto_now_add=False)
+    year = models.IntegerField(default=0)
     rate = models.CharField(max_length=10, default='')
     release = models.DateField(auto_now=False, auto_now_add=False, default='2000-01-01')
     runtime = models.IntegerField(default=0)
