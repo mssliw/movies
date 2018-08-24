82efd53581410a94ecebb0d10649d5e51bfd4520
mssliwinska <equinoxe@onet.eu>
2018-08-25 00:10:47 +0200

    minor setting change

--- movie_api/movie_api/settings.py
+++ movie_api/movie_api/settings.py
@@ -135,5 +135,3 @@ USE_TZ = True
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
