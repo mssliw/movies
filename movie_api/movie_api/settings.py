b29aa5ffd05cfd91c797300c53a91951d406f8a7
mssliwinska <equinoxe@onet.eu>
2018-08-24 23:21:19 +0200

    README update, settings adjustment

--- movie_api/movie_api/settings.py
+++ movie_api/movie_api/settings.py
@@ -132,4 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
